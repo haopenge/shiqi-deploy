f3abea91228b1af5fcadff051f932aa828939f32
liupenghao <liupenghao@huohua.cn>
2022-06-23 00:56:25 +0800

    镜像拉取secret添加

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # 登录docker仓库
 docker login --username=$ALI_DOCKER_USERNAME --password=$ALI_DOCKER_PASSWORD registry.cn-hangzhou.aliyuncs.com
 # 构建docker镜像
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.${BUILD_NUMBER} -f Dockerfile .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.1 -f Dockerfile .
 # push docker镜像
 docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.1
 
