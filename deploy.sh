fb4ff56bbbc31a850f50a46615b6cde7d0b3a5c0
liupenghao <liupenghao@huohua.cn>
2022-06-23 00:42:19 +0800

    docker image version更改

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ docker login --username=$ALI_DOCKER_USERNAME --password=$ALI_DOCKER_PASSWORD reg
 # 构建docker镜像
 docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.${BUILD_NUMBER} -f Dockerfile .
 # push docker镜像
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.${BUILD_NUMBER}
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.1
 
 # 发布服务
 #helm install shiqi-deploy chart/
