92c6169aad22dbc44833cd6b19f9a5f7be1479b3
liupenghao <liupenghao@huohua.cn>
2022-06-23 18:21:58 +0800

    移除打包版本

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # 登录docker仓库
 docker login --username=$ALI_DOCKER_USERNAME --password=$ALI_DOCKER_PASSWORD registry.cn-hangzhou.aliyuncs.com
 # 构建docker镜像
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER} -f Dockerfile .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0 -f Dockerfile .
 # push docker镜像
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER}
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0
 
 # 发布服务
 kubectl apply -f k8s.yaml
