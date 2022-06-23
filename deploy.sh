2f99ee2568ea537441080259cca7e75a00077386
liupenghao <liupenghao@huohua.cn>
2022-06-23 16:34:26 +0800

    部署引入版本号

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # 登录docker仓库
 docker login --username=$ALI_DOCKER_USERNAME --password=$ALI_DOCKER_PASSWORD registry.cn-hangzhou.aliyuncs.com
 # 构建docker镜像
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.1 -f Dockerfile .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER} -f Dockerfile .
 # push docker镜像
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:0.1.1
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER}
 
 # 发布服务
 #helm install shiqi-deploy chart/
