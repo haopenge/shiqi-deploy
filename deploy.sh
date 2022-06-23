a57ed5666bd9f88604009e9428d4d5bd12a84cdd
liupenghao <liupenghao@huohua.cn>
2022-06-23 17:25:50 +0800

    移除发布

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,5 @@ docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0 -f Do
 docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0
 
 # 发布服务
-kubectl apply -f k8s.yaml
 
 # 发布 chart
