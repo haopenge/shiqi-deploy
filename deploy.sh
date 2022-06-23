047aa6eea076ce1c10145496115aaa3c1877e2c4
liupenghao <liupenghao@huohua.cn>
2022-06-23 18:07:57 +0800

    引入打包版本

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # 登录docker仓库
 docker login --username=$ALI_DOCKER_USERNAME --password=$ALI_DOCKER_PASSWORD registry.cn-hangzhou.aliyuncs.com
 # 构建docker镜像
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0 -f Dockerfile .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER} -f Dockerfile .
 # push docker镜像
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER}
 
 # 发布服务
-kubectl delete -f k8s.yaml
+kubectl apply -f k8s.yaml
 
 # 发布 chart
