698365e045f538077613e870120fef3558d18c2a
liupenghao <liupenghao@huohua.cn>
2022-06-23 22:41:14 +0800

    add chart script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0 -f Do
 docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:1.0.0
 
 # 发布服务
-kubectl apply -f k8s.yaml
+#kubectl apply -f k8s.yaml
 
 # 发布 chart
+helm install shiqi-deploy chart
