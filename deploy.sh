6aff6d8a0e3b6450c220ff8ddb808dfd61c37c48
liupenghao <liupenghao@huohua.cn>
2022-06-24 01:05:33 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+echo "<<======================登录docker仓库===================>>"
+docker login --username=$ALI_DOCKER_USERNAME --password=$ALI_DOCKER_PASSWORD registry.cn-hangzhou.aliyuncs.com
+
+echo "<<======================构建docker镜像 ===================>>"
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER} -f Dockerfile .
+
+echo "<<======================push docker镜像 ===================>>"
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/shiqi-deploy:${BUILD_NUMBER}
+
+
+echo "<<======================修改helm、k8s.yaml中的镜像版本号 ===================>>"
+
+sed -i "s/build_number/${BUILD_NUMBER}/g" chart/Chart.yaml
+sed -i "s/build_number/${BUILD_NUMBER}/g" chart/values.yaml
+sed -i "s/build_number/${BUILD_NUMBER}/g" k8s.yaml
+
+echo "<<======================发布服务 ===================>>"
+kubectl apply -f k8s.yaml
+
